6eab15ab4b9593703e9c1e2e1e3b10cb5b6065cb
viktorrenkema <vrenkema@gmail.com>
2022-03-23 20:17:14 +0100

    Fix Vercel built error re: callback violating hooks rule

--- components/menu.jsx
+++ components/menu.jsx
@@ -358,9 +358,9 @@ export default function Menu(props) {
         {navigation.map((i, index) => (
           <MenuItemContainer
             key={i}
-            onTap={React.useCallback(() => {
+            onTap={() => {
               setNavSelection(index);
-            }, [])}
+            }}
           >
             <MenuH2Wrapper variants={stagger}>
               <MenuH2
